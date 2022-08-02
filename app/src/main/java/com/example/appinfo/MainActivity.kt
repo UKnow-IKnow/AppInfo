d10a1e24a6223600b512e764defff4db6f0b3a2a
UKnow-IKnow <sarojsahu014@gmail.com>
2022-08-02 18:05:49 +0530

    bug

--- app/src/main/java/com/example/appinfo/MainActivity.kt
+++ app/src/main/java/com/example/appinfo/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.appinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.appinfo.databinding.ActivityMainBinding
+import com.example.appinfo.repository.Repository
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var viewModel : MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+
+        viewModel.pushStatus("HIGHLYEXPORT", true,true,89,"1234gatshsjk")
+
+        viewModel.pushResponse.observe(this,{
+            response ->
+            binding.resultTv.text = response.body().toString()
+        })
     }
 }
